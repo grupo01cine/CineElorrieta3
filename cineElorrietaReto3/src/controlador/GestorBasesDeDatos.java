c0935030b43d8117fe0fbbc91baaa6361ef63d65
in1dw3 <in1dw3@HZ352016.elorrieta.int>
2023-02-08 19:16:58 +0100

    Ejemplo Con PreparedStatement

--- cineElorrietaReto3/src/controlador/GestorBasesDeDatos.java
+++ cineElorrietaReto3/src/controlador/GestorBasesDeDatos.java
@@ -2,6 +2,7 @@ package controlador;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -40,6 +41,80 @@ public class GestorBasesDeDatos {
 				int id = resultSet.getInt("Codigo");
 				String nombre = resultSet.getString("Nombre");
 				String direccion = resultSet.getString("Direccion");				
+
+				// Metemos los datos a Ejemplo
+				cine.setCodigo(id);
+				cine.setNombre(nombre);
+				cine.setDireccion(direccion);
+
+				ret.add(cine);
+
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+		}
+		return ret;
+	}
+	
+	public ArrayList<Cine> ejemploPrepared(int codigo, String nombreDado) {
+		ArrayList<Cine> ret = null;
+
+		Connection connection = null;
+
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		PreparedStatement  preparedStatement  = null;
+
+		try {
+			Class.forName(BBDDUtils.DRIVER_REMOTO);
+
+			connection = DriverManager.getConnection(BBDDUtils.URL_REMOTO, BBDDUtils.USER_REMOTO,
+					BBDDUtils.PASS_REMOTO);
+
+
+			// Montamos la SQL. Las ? se rellenan a continuacion
+			String sql = "SELECT * FROM Cine WHERE ";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setInt(1, codigo);
+			preparedStatement.setString(2, nombreDado);
+
+			while (resultSet.next()) {
+
+				// Si es necesario, inicializamos la lista
+				if (null == ret) {
+					ret = new ArrayList<Cine>();
+				}
+
+				Cine cine = new Cine();
+				// Sacamos las columnas del RS
+				int id = resultSet.getInt("Codigo");
+				String nombre = resultSet.getString("Nombre");
+				String direccion = resultSet.getString("Direccion");
 
 				// Metemos los datos a Ejemplo
 				cine.setCodigo(id);
