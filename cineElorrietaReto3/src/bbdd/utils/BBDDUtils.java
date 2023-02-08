53d2270538b54eb8102f5918565315bbfcc4a361
in1dw3 <in1dw3@HZ352016.elorrieta.int>
2023-02-08 17:48:36 +0100

    Conexion con base de datos servidor remoto terminada

--- cineElorrietaReto3/src/bbdd/utils/BBDDUtils.java
+++ cineElorrietaReto3/src/bbdd/utils/BBDDUtils.java
@@ -3,15 +3,24 @@ package bbdd.utils;
 // Configuraciones necesarias para la Base de Datos de MySql
 public class BBDDUtils {
 
+// ** Utils para conexion con Base de datos REMOTA **
 	// La URL donde esta la Base de Datos. Se descompone en:
 	// driver : bbd : // IP : Puerto / Schema
-	public static final String URL = "jdbc:mysql://localhost:3306/cineselorrieta";
+	public static final String URL_REMOTO = "jdbc:mysql://10.5.14.125:3306/CinesElorrieta";
 
 	// El Driver que vamos a usar
-	public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	
+	public static final String DRIVER_REMOTO = "com.mysql.cj.jdbc.Driver";
+
 	// Nombre y Pass de acceso a la Base de Datos
-	public static final String USER = "root";
-	public static final String PASS = "";
-	
+	public static final String USER_REMOTO = "userSelectsJava";
+	public static final String PASS_REMOTO = "Grupo01.reto";
+
+// ** Utils para trabajar de forma LOCAL **
+	//public static final String URL_LOCAL = "jdbc:mysql://localhost:3306/cineselorrieta";
+
+	//public static final String DRIVER_LOCAL = "com.mysql.cj.jdbc.Driver";
+
+	//public static final String USER_LOCAL = "root";
+	//public static final String PASS_LOCAL = "";
+
 }
---
+++ cineElorrietaReto3/src/controlador/GestorBasesDeDatos.java
@@ -0,0 +1,81 @@
+package controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import bbdd.pojos.Cine;
+import bbdd.utils.BBDDUtils;
+
+public class GestorBasesDeDatos {
+
+	public ArrayList<Cine> sacarTodosLosCines() {
+		ArrayList<Cine> ret = null;
+
+		Connection connection = null;
+
+		Statement statement = null;
+		ResultSet resultSet = null;
+
+		try {
+			Class.forName(BBDDUtils.DRIVER_REMOTO);
+
+			connection = DriverManager.getConnection(BBDDUtils.URL_REMOTO, BBDDUtils.USER_REMOTO, BBDDUtils.PASS_REMOTO);
+
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT * FROM Cine");
+
+			while (resultSet.next()) {
+
+				// Si es necesario, inicializamos la lista
+				if (null == ret) {
+					ret = new ArrayList<Cine>();
+				}
+
+				Cine cine = new Cine();
+				// Sacamos las columnas del RS
+				int id = resultSet.getInt("Codigo");
+				String nombre = resultSet.getString("Nombre");
+				String direccion = resultSet.getString("Direccion");				
+
+				// Metemos los datos a Ejemplo
+				cine.setCodigo(id);
+				cine.setNombre(nombre);
+				cine.setDireccion(direccion);
+
+				ret.add(cine);
+
+			}
+		} catch (SQLException sqle) {
+			System.out.println("Error con la BBDD - " + sqle.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error generico - " + e.getMessage());
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+			try {
+				if (statement != null)
+					statement.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (Exception e) {
+				// No hace falta
+			}
+			;
+		}
+		return ret;
+	}
+}
