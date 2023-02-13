1633d3fa539689b7a13089bb9707720ccbaf9a73
in1dw3 <in1dw3@HZ352016.elorrieta.int>
2023-02-13 21:32:35 +0100

    Actualizar POJO Cine

--- cineElorrietaReto3/src/bbdd/pojos/Cine.java
+++ cineElorrietaReto3/src/bbdd/pojos/Cine.java
@@ -1,6 +1,7 @@
 package bbdd.pojos;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Objects;
 
 /**
@@ -8,50 +9,62 @@ import java.util.Objects;
  * 
  * Relacion 1:N con la tabla Sala
  */
-public class Cine implements Serializable{
-	
+public class Cine implements Serializable {
+
 	private static final long serialVersionUID = -4280170159871175756L;
 
 	// Clave primaria
 	private int codigo = 0;
-	
+
 	// Atributos
 	private String nombre = "";
 	private String direccion = "";
-	
+
+	// Atributo de la relacion
+	// Relacion 1:N con la tabla Sala (en un cine hay N salas)
+	private ArrayList<Cine> cines = null;
+
 	public int getCodigo() {
 		return codigo;
 	}
-	
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public String getDireccion() {
 		return direccion;
 	}
-	
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
-	
+
+	public ArrayList<Cine> getCines() {
+		return cines;
+	}
+
+	public void setCines(ArrayList<Cine> cines) {
+		this.cines = cines;
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(codigo, direccion, nombre);
+		return Objects.hash(cines, codigo, direccion, nombre);
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -61,15 +74,15 @@ public class Cine implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Cine other = (Cine) obj;
-		return codigo == other.codigo && Objects.equals(direccion, other.direccion)
-				&& Objects.equals(nombre, other.nombre);
+		return Objects.equals(cines, other.cines) && codigo == other.codigo
+				&& Objects.equals(direccion, other.direccion) && Objects.equals(nombre, other.nombre);
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Cine [codigo=" + codigo + ", nombre=" + nombre + ", direccion=" + direccion + "]";
+		return "Cine [codigo=" + codigo + ", nombre=" + nombre + ", direccion=" + direccion + ", cines=" + cines + "]";
 	}
 
 	
-	
+
 }
