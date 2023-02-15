2bafd20ba0016c5c76389e33397d58c5bb5ac586
in1dw3 <in1dw3@HZ352016.elorrieta.int>
2023-02-15 16:10:09 +0100

    Creacion del Ticket

--- cineElorrietaReto3/src/controlador/GestorFicheros.java
+++ cineElorrietaReto3/src/controlador/GestorFicheros.java
@@ -28,15 +28,17 @@ import bbdd.utils.BBDDUtils;
 
 public class GestorFicheros {
 
-	//private static final String RUTA_CARPETA = "C://Users//in1dw3//eclipse-workspace//BBDDyFicheros//src//BackUps//";
+	// private static final String RUTA_CARPETA =
+	// "C://Users//in1dw3//eclipse-workspace//BBDDyFicheros//src//BackUps//";
 	private static final String RUTA_CARPETA = System.getProperty("user.home") + "/Desktop/";
-	//private static final File RUTA_CARPETA = FileSystemView.getFileSystemView().getHomeDirectory();
+	// private static final File RUTA_CARPETA =
+	// FileSystemView.getFileSystemView().getHomeDirectory();
 
 	File file = null;
 
-	// Leer 
-	//FileReader fileReader = null;
-	//BufferedReader bufferedReader = null;
+	// Leer
+	// FileReader fileReader = null;
+	// BufferedReader bufferedReader = null;
 
 	// Escribir
 	FileWriter fileWriter = null;
@@ -45,8 +47,9 @@ public class GestorFicheros {
 	public GestorFicheros() {
 
 	}
-	
+
 	public ArrayList<Entrada> PRUEBA_SacarEntradas() {
+		// TODO Cambiar codEntrada
 		ArrayList<Entrada> ret = null;
 
 		Connection connection = null;
@@ -60,23 +63,24 @@ public class GestorFicheros {
 			connection = DriverManager.getConnection(BBDDUtils.URL_REMOTO, BBDDUtils.USER_REMOTO,
 					BBDDUtils.PASS_REMOTO);
 
+			String sql = "SELECT" + " e.Codigo AS CodEntrada," + " e.Fecha_Compra AS FechaEntrada,"
+					+ " e.Hora_Compra AS HoraEntrada," + " p.Codigo AS CodProyeccion," + " p.Fecha AS FechaProyeccion,"
+					+ " p.Horario AS HorarioProyeccion," + " p.Precio AS PrecioProyeccion," + " s.Codigo AS CodSala,"
+					+ " s.Numero_Sala AS NomSala," + " c.Codigo AS CodCine," + " c.Nombre AS NomCine,"
+					+ " c.Direccion AS DirCine," + " pe.Codigo AS CodPelicula,"
+					+ " pe.CosteProduccion AS CostePelicula," + " pe.Duracion AS DuracionPelicula,"
+					+ " pe.Genero AS GeneroPelicula," + " pe.Titulo AS TituloPelicula," + " cl.Codigo AS CodCliente,"
+					+ " cl.DNI AS DNICliente," + " cl.Nombre AS NomCliente," + " cl.Apellido AS ApellidoCliente,"
+					+ " cl.Sexo AS SexoCliente" + " FROM" + " Entrada AS e"
+					+ " JOIN Proyeccion AS p ON e.Proyeccion_Codigo = p.Codigo"
+					+ " JOIN Cliente AS cl ON e.Cliente_Codigo = cl.Codigo"
+					+ " JOIN Pelicula AS pe ON p.Pelicula_Codigo = pe.Codigo"
+					+ " JOIN Sala AS s ON p.Sala_Codigo = s.Codigo" + " JOIN Cine AS c ON s.Cine_Codigo = c.Codigo"
+					+ " WHERE" + " e.Codigo = 1";
+
 			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT * FROM Cine");
-			
-			String sql = "SELECT e.Codigo AS CodEntrada, e.Fecha AS FechaEntrada, e.Horario AS HorarioEntrada, "
-					+ "p.Codigo AS CodProyeccion, p.Fecha AS FechaProyeccion, p.Horario AS HorarioProyeccion, p.Precio AS PrecioProyeccion, "
-					+ "s.Codigo AS CodSala, s.Nombre AS NomSala, "
-					+ "c.Codigo AS CodCine, c.Nombre AS NomCine, c.Direccion AS DirCine, "
-					+ "pe.Codigo AS CodPelicula, pe.Coste AS CostePelicula, pe.Duraccion AS DuracionPelicula, pe.Genero AS GeneroPelicula, pe.Titulo AS TituloPelicula,"
-					+ "cl.Codigo AS CodCliente, cl.DNI AS DNICliente, cl.Nombre AS NomCliente, cl.Apellido AS ApellidoCliente, cl.Sexo AS SexoCliente"
-					+ "FROM Entrada AS e "
-					+ "JOIN Proyeccion AS p ON e.ProyeccionCodigo = p.Codigo"
-					+ "JOIN Cliente AS cl ON e.ClienteCodigo = cl.Codigo"
-					+ "JOIN Pelicula AS pe ON p.PeliculaCodigo = pe.Codigo"
-					+ "JOIN Sala AS s ON p.SalaCodigo = s.Codigo"
-					+ "JOIN Cine AS c ON s.CineCodigo = c.Codigo"
-					+ "WHERE CodEntrada = 1";
-			
+			resultSet = statement.executeQuery(sql);
+
 			while (resultSet.next()) {
 
 				// Si es necesario, inicializamos la lista
@@ -85,48 +89,50 @@ public class GestorFicheros {
 				}
 
 				Entrada entrada = new Entrada();
-				
-				entrada.setCodigo(resultSet.getInt(1));		
-				entrada.setFechaCompra(resultSet.getDate(2)); // En el POJO guardamos SQL Date por lo que no hace falta comvertirla
+
+				entrada.setCodigo(resultSet.getInt(1));
+				entrada.setFechaCompra(resultSet.getDate(2)); // En el POJO guardamos SQL Date por lo que no hace falta
+																// comvertirla
 				entrada.setHoraCompra(resultSet.getTime(3).toLocalTime());
-				
-				Proyeccion proyeccion = new Proyeccion();				
+
+				Proyeccion proyeccion = new Proyeccion();
 				proyeccion.setCodigo(resultSet.getInt(4));
 				proyeccion.setFecha(resultSet.getDate(5));
 				proyeccion.setHorario(resultSet.getTime(6).toLocalTime());
 				proyeccion.setPrecio(resultSet.getDouble(7));
-				
-				Sala sala = new Sala();				
+
+				Sala sala = new Sala();
 				sala.setCodigo(resultSet.getInt(8));
-				sala.setNombre(resultSet.getString(9));				
-				
+				sala.setNombre(resultSet.getString(9));
+
 				Cine cine = new Cine();
 				cine.setCodigo(resultSet.getInt(10));
 				cine.setNombre(resultSet.getString(11));
 				cine.setDireccion(resultSet.getString(12));
-				
+
 				sala.setCine(cine);
-								
+
 				proyeccion.setSala(sala);
-										
+
 				Pelicula pelicula = new Pelicula();
 				pelicula.setCodigo(resultSet.getInt(13));
 				pelicula.setCoste(resultSet.getDouble(14));
 				pelicula.setDuracion(resultSet.getTime(15).toLocalTime());
 				pelicula.setGenero(resultSet.getString(16));
-				pelicula.setTitulo(resultSet.getString(17));			
-											
+				pelicula.setTitulo(resultSet.getString(17));
+
 				proyeccion.setPelicula(pelicula);
-				
+
 				Cliente cliente = new Cliente();
 				cliente.setCodigo(resultSet.getInt(18));
 				cliente.setDni(resultSet.getString(19));
 				cliente.setNombre(resultSet.getString(20));
 				cliente.setApellido(resultSet.getString(21));
 				cliente.setSexo(resultSet.getString(22));
-				
+
 				entrada.setCliente(cliente);
-				
+				entrada.setProyeccion(proyeccion);
+
 				ret.add(entrada);
 
 			}
@@ -161,32 +167,40 @@ public class GestorFicheros {
 	}
 
 	public void crearNuevoTicket(ArrayList<Entrada> entradaDada) {
+		if (null == entradaDada) {
+			return;
+		}
+
 		// Vamos a añadirle la fecha de creacion del Ticket al nombre del Ticket
 		Date date = new Date();
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+		// SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		String fech = dateFormat.format(date);
 
-		File fich = new File(RUTA_CARPETA + "Ticket_Cine_" + fech + ".txt");
+		File fich = new File(RUTA_CARPETA + "Entradas_Cine_" + fech + ".txt");
 
 		try {
 			fileWriter = new FileWriter(fich);
 			printWriter = new PrintWriter(fileWriter);
 
 			for (Entrada entrada : entradaDada) {
+				printWriter.println("Entrada " + entrada.getCodigo());
 				printWriter.println(" Datos Cine");
-				printWriter.println("	- Cine: " + entrada.getProyeccion().getSala().getCine().getNombre() + " - Direccion: " + entrada.getProyeccion().getSala().getCine().getDireccion());
+				printWriter.println("	- Cine: " + entrada.getProyeccion().getSala().getCine().getNombre());
+				printWriter.println("   - Direccion: " + entrada.getProyeccion().getSala().getCine().getDireccion());
 				printWriter.println("	- Pelicula: " + entrada.getProyeccion().getPelicula().getTitulo());
 				printWriter.println("	  > Duraccion: " + entrada.getProyeccion().getPelicula().getDuracion());
 				printWriter.println("	  > Gemero: " + entrada.getProyeccion().getPelicula().getGenero());
 				printWriter.println("	- Sala: " + entrada.getProyeccion().getSala().getNombre());
-				printWriter.println("	- Precio: " + entrada.getProyeccion().getPrecio());
+				printWriter.println("	- Precio: " + entrada.getProyeccion().getPrecio() + "€");
 				printWriter.println(" Datos Cliente");
 				printWriter.println("	- DNI: " + entrada.getCliente().getDni());
 				printWriter.println("	- Nombre: " + entrada.getCliente().getNombre());
 				printWriter.println("	- Apellido: " + entrada.getCliente().getApellido());
 				printWriter.println("	- Sexo: " + entrada.getCliente().getSexo());
 				printWriter.println(" Fecha compra: " + entrada.getFechaCompra());
-				
+				printWriter.println(" Hora compra: " + entrada.getHoraCompra());
+				printWriter.println("");
 			}
 
 		} catch (IOException e) {
@@ -201,5 +215,4 @@ public class GestorFicheros {
 			}
 		}
 	}
-
 }
