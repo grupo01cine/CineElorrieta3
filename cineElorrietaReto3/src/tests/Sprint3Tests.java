c48691a9cbdf79d72475297665e99f2cd0394078
in1dw3 <in1dw3@HZ352015.elorrieta.int>
2023-02-24 15:53:08 +0100

    JUnit resumen compra

--- cineElorrietaReto3/src/tests/Sprint3Tests.java
+++ cineElorrietaReto3/src/tests/Sprint3Tests.java
@@ -32,6 +32,59 @@ class Sprint3Tests {
 	// ** TESTS RESUMEN COMPRA **
 	@Test
 	void testCalculoResumen() {
+		JTable tablaResumen = new JTable();
+		ArrayList<Proyeccion> proyeccionesSeleccionadas = new ArrayList<Proyeccion>();
+		
+
+		Proyeccion proyeccion1 = new Proyeccion();
+		proyeccion1.setCodigo(1);
+		LocalTime time = LocalTime.now();
+		proyeccion1.setHorario(time);
+		proyeccion1.setPrecio(5.5);
+		
+		Pelicula pelicula1 = new Pelicula();
+		pelicula1.setTitulo("Handia");
+		proyeccion1.setPelicula(pelicula1);
+		
+		Sala sala1 = new Sala();
+		sala1.setCodigo(1);
+		proyeccion1.setSala(sala1);
+		
+		proyeccionesSeleccionadas.add(proyeccion1);
+		
+		Proyeccion proyeccion2 = new Proyeccion();
+		proyeccion2.setCodigo(1);
+		proyeccion2.setHorario(time);
+		proyeccion2.setPrecio(6.5);
+
+		Pelicula pelicula2 = new Pelicula();
+		pelicula2.setTitulo("El cisne negro");
+		proyeccion2.setPelicula(pelicula2);
+
+		Sala sala2 = new Sala();
+		sala2.setCodigo(1);
+		proyeccion2.setSala(sala2);
+
+		proyeccionesSeleccionadas.add(proyeccion2);
+
+		tablaResumen.setModel(
+				new DefaultTableModel(new Object[][] {}, new String[] { "T\u00EDtulo", "Sesion", "Sala", "Precio" }));
+		DefaultTableModel model = (DefaultTableModel) tablaResumen.getModel();
+		model.setRowCount(0);
+
+		for (Proyeccion proyec : proyeccionesSeleccionadas) {
+			model.addRow(new String[] { proyec.getPelicula().getTitulo(), proyec.getHorario().toString(),
+					Integer.toString(proyec.getSala().getCodigo()), proyec.getPrecio().toString() });
+		}
+		
+		
+		
+		
+		assertEquals(12.0, metodosVentanas.sacarPrecioTotal(tablaResumen));
+		
+		
+		
+		
 //		fail("Not yet implemented");
 	}
 
